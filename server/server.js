8b234149402a8b805abc27c384528e0970f4981b
cheuyin <yinstanleycheung@gmail.com>
2023-11-02 21:40:24 -0700

    Make port pull from environmental variables and make local default port 8000

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 const app = express();
-const port = 3000;
 
 import StudentRouter from "./routes/StudentRouter.js"
 
 app.use("/api/students", StudentRouter);
 
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
