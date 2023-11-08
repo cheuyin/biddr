d23f071cf1c7157eb63080324205b91003953427
Sunny Nie <sunnymingnie@gmail.com>
2023-11-08 15:22:03 -0800

    start on wallets, can create and delete wallets

--- server/server.js
+++ server/server.js
@@ -6,14 +6,15 @@ import StudentRouter from "./routes/StudentRouter.js";
 import CommunityRouter from "./routes/CommunityRouter.js";
 import AppUserRouter from "./routes/AppUserRouter.js"
 import UserRouter from "./routes/user.js";
+import WalletRouter from "./routes/WalletRouter.js";
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/students", StudentRouter);
 app.use("/api/communities", CommunityRouter);
 app.use("/api/users", AppUserRouter);
-
 app.use("/auth/", UserRouter);
+app.use("/api/wallets", WalletRouter);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
