5300fc1c961b9a1f9240e997a354c1449d747161
Sunny Nie <sunnymingnie@gmail.com>
2023-11-13 15:52:03 -0800

    add get and put

--- server/routes/AppUserRouter.js
+++ server/routes/AppUserRouter.js
@@ -2,7 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-import { GetAppUserByEmail, PostAppUser } from "../controllers/AppUserController.js";
+import {
+  GetAppUserByEmail,
+  PostAppUser,
+} from "../controllers/AppUserController.js";
 
 router.get("/:email", GetAppUserByEmail);
 router.post("/", PostAppUser);
--- server/routes/WalletRouter.js
+++ server/routes/WalletRouter.js
@@ -2,9 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
-import { DeleteWallet, PostWallet } from "../controllers/WalletController.js";
+import {
+  DeleteWallet,
+  PostWallet,
+  PutWalletBalance,
+} from "../controllers/WalletController.js";
+import { GetAllWallets, GetWallet } from "../controllers/WalletController.js";
 
 router.post("/", PostWallet);
 router.delete("/", DeleteWallet);
+router.get("/:email/:name", GetWallet);
+router.get("/:email", GetAllWallets);
+router.put("/:email/:name", PutWalletBalance);
 
 export default router;
