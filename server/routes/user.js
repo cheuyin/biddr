ddad841f55d9971bed887ca6d220639602b9e4e5
cheuyin <yinstanleycheung@gmail.com>
2023-11-13 16:23:52 -0800

    Save refresh token in database if authentication is successful

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ import express from "express";
 import { QueryAppUserByEmail } from "../services/AppUserTable.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import query from "../db.js";
 
 const router = express.Router();
 
@@ -17,33 +18,69 @@ router.post("/signin", async (req, res, next) => {
     let user;
     try {
         user = await QueryAppUserByEmail(email);
-        if (!user) {
-            return res
-                .status(404)
-                .json({ error: "User with that email does not exist." });
-        }
     } catch (error) {
         return res.status(400).json({ error: error.message });
     }
 
+    if (!user) {
+        return res
+            .status(404)
+            .json({ error: "User with that email does not exist." });
+    }
+
     const isCorrectPassword = bcrypt.compareSync(password, user.hashedpassword);
 
-    if (isCorrectPassword) {
-        const token = jwt.sign(
-            {
-                email: email,
-            },
-            process.env.JWT_KEY,
-            {
-                expiresIn: "2 days",
-            }
-        );
-        return res
-            .status(200)
-            .json({ message: "Passwords match!", token: token });
-    } else {
+    if (!isCorrectPassword) {
         return res.status(401).json({ error: "Password is incorrect." });
     }
+
+    // create the json web tokens
+    const accessToken = jwt.sign(
+        {
+            email: user.email,
+        },
+        process.env.JWT_ACCESS_TOKEN_KEY,
+        {
+            expiresIn: 15 * 60,
+        }
+    );
+    const refreshToken = jwt.sign(
+        {
+            email: user.email,
+        },
+        process.env.JWT_REFRESH_TOKEN_KEY,
+        {
+            expiresIn: "3d",
+        }
+    );
+
+    // save refresh tokens in the database
+    try {
+        const result = await query(
+            "sELECT * FROM refreshtokens WHERE email = $1",
+            [user.email]
+        );
+        if (result.length === 0) {
+            await query(
+                "INSERT INTO refreshtokens (email, token) VALUES ($1, $2);",
+                [user.email, refreshToken]
+            );
+        } else {
+            await query(
+                "UPDATE refreshtokens SET token = $1 WHERE email = $2",
+                [user.refreshToken, user.email]
+            );
+        }
+    } catch (error) {
+        return res.status(500).json({
+            error: "There was an error saving refresh tokens in the database.",
+        });
+    }
+
+    // return success result + access token to the user
+    return res
+        .status(200)
+        .json({ message: "Passwords match!", accessToken: accessToken });
 });
 
 export default router;
