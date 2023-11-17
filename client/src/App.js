a97bdd5df0ce004a170bd953e22394a8bd791523
cheuyin <yinstanleycheung@gmail.com>
2023-11-17 11:59:45 -0800

    Create a wrapper for the sign in / sign up pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInForm from "./components/SignInForm.js";
 import SignUpForm from "./components/SignUpForm.js";
 import HomePage from "./pages/HomePage.js";
+import AuthPage from "./pages/AuthPage.js";
 
 function App() {
     return (
@@ -11,8 +12,11 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="/signin" element={<SignInForm />} />
-                    <Route path="/signup" element={<SignUpForm />} />
+                    <Route path="/auth" element={<AuthPage />}>
+                        <Route path="" element={<SignInForm />} />
+                        <Route path="signin" element={<SignInForm />} />
+                        <Route path="signup" element={<SignUpForm />} />
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </ChakraProvider>
--- client/src/components/SignInForm.js
+++ client/src/components/SignInForm.js
@@ -1,13 +1,10 @@
 import {
-    Container,
-    Box,
     Heading,
     Input,
     FormErrorMessage,
     FormControl,
     FormLabel,
     Button,
-    Text,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { useState, useContext } from "react";
@@ -49,68 +46,56 @@ const SignInForm = () => {
     };
 
     return (
-        <Container maxW="md" mt={10}>
-            <Box
-                border="1px"
-                p={4}
-                borderRadius="md"
-                boxShadow="sm"
-                borderColor={"gray.200"}
-            >
-                <Heading as="h1" size="lg" m={4} textAlign={"center"}>
-                    Sign In
-                </Heading>
-                <Text>{"LOGGED IN USER: " + auth.email}</Text>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <FormControl isInvalid={errors.email}>
-                        <FormLabel htmlFor="email">Email</FormLabel>
-                        <Input
-                            id="email"
-                            {...register("email", {
-                                required: "Please enter your email.",
-                                pattern: {
-                                    value: /\S+@\S+\.\S+/,
-                                    message:
-                                        "Please enter a valid email address.",
-                                },
-                                maxLength: {
-                                    value: 256,
-                                    message:
-                                        "Your email cannot be more than 256 characters long.",
-                                },
-                            })}
-                        />
-                        <FormErrorMessage>
-                            {errors.email?.message}
-                        </FormErrorMessage>
-                    </FormControl>
-                    <FormControl isInvalid={errors.password}>
-                        <FormLabel htmlFor="password">Password</FormLabel>
-                        <Input
-                            id="password"
-                            type="password"
-                            {...register("password", {
-                                required: "Please enter your password.",
-                            })}
-                        />
-                        <FormErrorMessage>
-                            {errors.password?.message}
-                        </FormErrorMessage>
-                    </FormControl>
+        <>
+            <Heading as="h1" size="lg" m={4} textAlign={"center"}>
+                Sign In
+            </Heading>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl isInvalid={errors.email}>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                        id="email"
+                        {...register("email", {
+                            required: "Please enter your email.",
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "Please enter a valid email address.",
+                            },
+                            maxLength: {
+                                value: 256,
+                                message:
+                                    "Your email cannot be more than 256 characters long.",
+                            },
+                        })}
+                    />
+                    <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={errors.password}>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input
+                        id="password"
+                        type="password"
+                        {...register("password", {
+                            required: "Please enter your password.",
+                        })}
+                    />
+                    <FormErrorMessage>
+                        {errors.password?.message}
+                    </FormErrorMessage>
+                </FormControl>
 
-                    <Button
-                        isLoading={isSubmitting}
-                        loadingText="Submitting"
-                        spinnerPlacement="start"
-                        width="100%"
-                        mt={4}
-                        type="submit"
-                    >
-                        Submit
-                    </Button>
-                </form>
-            </Box>
-        </Container>
+                <Button
+                    isLoading={isSubmitting}
+                    loadingText="Submitting"
+                    spinnerPlacement="start"
+                    width="100%"
+                    mt={4}
+                    type="submit"
+                >
+                    Submit
+                </Button>
+            </form>
+        </>
     );
 };
 
