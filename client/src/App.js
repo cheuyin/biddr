2274b7d384d56140c701136033ee0fdbd239e624
cheuyin <yinstanleycheung@gmail.com>
2023-11-17 17:08:38 -0800

    Add page not found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignInForm from "./components/SignInForm.js";
 import SignUpForm from "./components/SignUpForm.js";
 import HomePage from "./pages/HomePage.js";
 import AuthPage from "./pages/AuthPage.js";
+import PageNotFound from "./pages/PageNotFound.js";
 
 function App() {
     return (
@@ -13,10 +14,11 @@ function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/auth" element={<AuthPage />}>
-                        <Route path="" element={<SignInForm />} />
+                        <Route path="" element={<Navigate to="signin" />} />
                         <Route path="signin" element={<SignInForm />} />
                         <Route path="signup" element={<SignUpForm />} />
                     </Route>
+                    <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
         </ChakraProvider>
