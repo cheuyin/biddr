8bbb7611b6267b6b071b5406fd7953024d01c73f
Matias Gauvin <matiasgauvin@gmail.com>
2023-11-19 15:26:02 -0800

    Added sidebar basic UI and created Layout Component w/ sidebar

---
+++ client/src/components/BiddrLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BiddrSidebar from "./BiddrSidebar.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function BiddrLayout({ children }) {
+    return (
+      <ChakraProvider>
+        <BiddrSidebar>
+        {children}
+        </BiddrSidebar>
+      </ChakraProvider>
+    );
+  }
