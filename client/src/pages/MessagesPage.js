509d7b7a83543dbfa2efe10f52660c5ce2317b4b
cheuyin <yinstanleycheung@gmail.com>
2023-11-22 17:31:10 -0800

    Pull chats that a user belongs to and display on page

--- client/src/pages/MessagesPage.js
+++ client/src/pages/MessagesPage.js
@@ -1,20 +1,37 @@
+import { useEffect, useState } from "react";
 import { Container, Heading, Text } from "@chakra-ui/react";
 import useAuth from "../hooks/useAuth";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 const MessagesPage = () => {
     const { auth } = useAuth();
+    const axiosPrivate = useAxiosPrivate();
+    const [chatNames, setChatNames] = useState([]);
+
+    console.log(auth.email);
+
+    useEffect(() => {
+        const getChatData = async () => {
+            if (auth.email) {
+                try {
+                    const response = await axiosPrivate.get(
+                        "/api/chats/users/" + auth.email
+                    );
+                    setChatNames(response.data.data);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        };
+        getChatData();
+    }, [auth.email, axiosPrivate]);
 
     return (
-        <Container
-            maxW="2xl"
-            centerContent
-            backgroundColor={"teal.100"}
-            p={4}
-            my={4}
-        >
-            <Heading>Messages Page</Heading>
-            <Text>If You're Reading This, You're Authenticated. </Text>
-            <Text>Email: {auth.email} </Text>
+        <Container maxW="100%" height={'90vh'} centerContent backgroundColor={"teal.100"}>
+            <Heading>Chats </Heading>
+            {chatNames.map((chat) => (
+                <Text key={chat.chatid}>{chat.chatname}</Text>
+            ))}
         </Container>
     );
 };
